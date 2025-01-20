f1fb5f005f9f275df90bfe53e636f10599fbed23
oTriangulo <guusswb@gmail.com>
2025-01-20 16:01:16 -0300

    Commit 1 - Começando
    Adicionando as páginas importantes e trabalhando no Header da página, assim como configuração do react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '.components/Header.js';
+import Navbar from '.components/Navbar.js';
+
+// Páginas da Wiki
+import Home from './pages/Home';
+import Personagens from './pages/Personagens';
+import Lugares from './pages/Lugares';
+import Elementos from './pages/Elementos';
+import OMundo from './pages/OMundo';
+import Galeria from './pages/Galeria';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Header />
+      <Navbar />
+      <div className="content">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/personagens" component={Personagens} />
+          <Route path="/lugares" component={Lugares} />
+          <Route path="/elementos" component={Elementos} />
+          <Route path="/omundo" component={OMundo} />
+          <Route path="/galeria" component={Galeria} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Personagens.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Personagens = () => {
+  return (
+    <div className="personagens">
+      <h2>Personagens</h2>
+      <p>Lista de personagens do livro.</p>
+    </div>
+  );
+};
+
+export default Personagens;
